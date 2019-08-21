34c54bacd26c3b28192491c8a74c8d0cbd6e07fe
protyliss <protyliss@gmail.com>
2019-08-21 15:44:51 +0900

    add dist

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./static-log"));
+__export(require("./log"));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsa0NBQTZCO0FBQzdCLDJCQUFzQiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vc3RhdGljLWxvZyc7XHJcbmV4cG9ydCAqIGZyb20gJy4vbG9nJztcclxuIl19
---
+++ dist/static-log.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const log_1 = require("./log");
+exports.default = new log_1.Log();
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN0YXRpYy1sb2cudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSwrQkFBMEI7QUFFMUIsa0JBQWUsSUFBSSxTQUFHLEVBQUUsQ0FBQyIsImZpbGUiOiJzdGF0aWMtbG9nLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtMb2d9IGZyb20gXCIuL2xvZ1wiO1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgbmV3IExvZygpO1xyXG4iXX0=
